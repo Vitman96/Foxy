3a8fbedb3d3cbf3d81efd705bf784e5fec80c398
Vitman96 <vkoneswarathsan@gmail.com>
2020-04-14 19:45:02 +0200

    Enemy
    - add Enemy to Level 2
    - add sound for Enemy

---
+++ 2D_Firts_Try/Assets/Scripts/enemytHitSound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemytHitSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource audioSource;
+    public Animator animator;
+     void OnTriggerEnter2D(Collider2D col)
+    {
+        animator.SetBool("isHurt",true); 
+        Invoke("SetBoolBack", 0.25f);
+        if(ScoreTextScribt.coinAmount >0){
+            ScoreTextScribt.coinAmount -=1;
+
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+        //animator.SetBool("isHurt",false); 
+    }
+
+   private void SetBoolBack()
+   {
+       animator.SetBool("isHurt",false); 
+   }
+}
