0d98facea6c86739670144d9ba7abac41743cf32
Vitman96 <vkoneswarathsan@gmail.com>
2020-04-15 22:20:20 +0200

    Final Out

---
+++ 2D_Firts_Try/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void playGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public void quitGame(){
+         Application.Quit();
+    }
+  
+}
